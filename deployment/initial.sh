d4970b07c1dda21a4741760a2f85c101fe01c918
Omar Siam <simar@gmx.net>
2021-09-20 17:12:29 +0200

    Change how the data is fetched to be a bit more like it will be using ARCHE. Update data paths within current directory structure in data repo.

--- deployment/initial.sh
+++ deployment/initial.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp -Rv ./deployment/* ../../
-if [ -f package.json ]
+if [ -f package.json ]
 then npm install
 fi
 cd ../
@@ -9,7 +9,10 @@ cd ../
 if [ -f redeploy.settings.dist ]
 then mv redeploy.settings.dist redeploy.settings
 fi
-git clone git@gitlab.com:acdh-oeaw/voice/voice_data.git
+curl -O --header "PRIVATE-TOKEN: q3_c-GvQ_sV6upEw1xJE" "https://gitlab.com/api/v4/projects/21073173/repository/archive.tar"
+tar -xf archive.tar
+rm archive.tar
+mv voice_data* voice_data
 pushd lib/custom
 curl -LO https://repo1.maven.org/maven2/net/sf/saxon/Saxon-HE/10.5/Saxon-HE-10.5.jar
 popd
--- deployment/redeploy.sh
+++ deployment/redeploy.sh
@@ -26,38 +26,4 @@ git checkout master
 popd
 #-------------------------------------
 
-#------ Update content data from git repository
-echo Updating voice_data
-pushd voice_data
-git pull
-ret=$?
-if [ $ret != "0" ]; then exit $ret; fi
-if [ "$onlytags"x = 'truex' ]
-then
-dataversion=$(git describe --tags --always)
-echo checking out data ${dataversion}
-git -c advice.detachedHead=false checkout ${dataversion}
-who=$(git show -s --format='%cN')
-when=$(git show -s --format='%as')
-message=$(git show -s --format='%B')
-revisionDesc=$(sed ':a;N;$!ba;s/\n/\\n/g' <<EOF
-<revisionDesc>
-  <change n="$dataversion" who="$who" when="$when">
-$message
-   </change>
-</revisionDesc>
-EOF
-)
-fi
-git checkout master
-popd
-if [ "$onlytags"x = 'truex' ]
-then
-pushd webapp/voice-clariah-api
-find ./ -type f -and \( s-name '*.xq' -or -name '*.js' -or -name '*.html' \) -not \( -path './node_modules/*' -or -path './cypress/*' \) -exec sed -i "s~\@data-version@~$dataversion~g" {} \;
-popd
-fi
 ./execute-basex-batch.sh deploy-voice-content
-pushd voice_data
-git reset --hard
-popd
