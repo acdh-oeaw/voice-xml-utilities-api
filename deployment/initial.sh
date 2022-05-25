0d7bdf4c47029f5afb23a45966dc4a399c8cc486
Omar Siam <Omar.Siam@oeaw.ac.at>
2022-05-25 13:47:05 +0200

    Fix: path is different for AutoDevOps build

--- deployment/initial.sh
+++ deployment/initial.sh
@@ -9,7 +9,7 @@ cd ../
 if [ -f redeploy.settings.dist ]
 then mv redeploy.settings.dist redeploy.settings
 fi
-node collection_download_script.cjs --flat --targetDir voice-data/xml
+node ${1:-.}/collection_download_script.cjs --flat --targetDir voice-data/xml
 if [ "${STACK}x" = "x" ]; then
 pushd lib/custom
 curl -LO https://repo1.maven.org/maven2/net/sf/saxon/Saxon-HE/11.3/Saxon-HE-11.3.jar
